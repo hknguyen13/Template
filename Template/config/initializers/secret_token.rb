40a6cbe2dce2662579536eb4139f35a434832faf
Huu Nguyen <hknguyen13@gmail.com>
2013-11-29 23:12:09 -0500

    init commit

---
+++ Template/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Template::Application.config.secret_key_base = 'c04b97bc74408dc2ab1b8ad3ec900060f86558c7c3ec265feecc198c3b153b621cdbee61b4e478fded6d27e42cc0a7e9e6a76bb709fef4758783fb4fb3007827'
